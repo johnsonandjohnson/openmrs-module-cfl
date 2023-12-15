642316b28def5e6221fc90ac9ad78a61069f0130
Ruchniewicz, Dawid [RNDBE NON-J&J] <DRuchni1@its.jnj.com>
2023-12-15 08:25:08 -0500

    Pull request #41: AGRE-3147: Improved breadcrumb navigation
    Merge in ITX-BPV/openmrs-module-cfl from feature/AGRE-3147 to development
    
    Squashed commit of the following:
    
    commit 4df8b182d5af695ec7d16f130228442b12690201
    Author: druchniewicz <druchniewicz@soldevelo.com>
    Date:   Wed Dec 13 17:33:18 2023 +0100
    
        AGRE-3147: Removed unused and broken pages from Configure Metadata page
    
    commit 006254b962c0f99db022197ba4de2646d25ed1b2
    Author: druchniewicz <druchniewicz@soldevelo.com>
    Date:   Tue Dec 12 09:47:29 2023 +0100
    
        AGRE-3147: Improved breadcrumbs appearance and behavior

--- api/src/main/java/org/openmrs/module/cfl/api/util/PatientDashboardAppsConstants.java
+++ api/src/main/java/org/openmrs/module/cfl/api/util/PatientDashboardAppsConstants.java
@@ -11,72 +11,117 @@
 package org.openmrs.module.cfl.api.util;
 
 public final class PatientDashboardAppsConstants {
-    public static final String CFL_PATIENT_DASHBOARD_IMPROVEMENTS_APP = "cfl.patientDashboard.improvements";
+  public static final String CFL_PATIENT_DASHBOARD_IMPROVEMENTS_APP =
+      "cfl.patientDashboard.improvements";
 
-    public static final String CFL_RELATIONSHIPS_APP = "cfl.relationships";
+  public static final String CFL_RELATIONSHIPS_APP = "cfl.relationships";
 
-    public static final String CFL_LATESTOBSFORCONCEPTLIS_APP = "cfl.latestobsforconceptlis";
+  public static final String CFL_LATESTOBSFORCONCEPTLIS_APP = "cfl.latestobsforconceptlis";
 
-    public static final String APPOINTMENTSCHEDULINGUI_HOME_APP = "appointmentschedulingui.homeApp";
+  public static final String APPOINTMENTSCHEDULINGUI_HOME_APP = "appointmentschedulingui.homeApp";
 
-    public static final String SCHEDULING_APPOINTMENT_APP = "appointmentschedulingui.schedulingAppointmentApp";
+  public static final String SCHEDULING_APPOINTMENT_APP =
+      "appointmentschedulingui.schedulingAppointmentApp";
 
-    public static final String REQUEST_APPOINTMENT_APP = "appointmentschedulingui.requestAppointmentApp";
+  public static final String REQUEST_APPOINTMENT_APP =
+      "appointmentschedulingui.requestAppointmentApp";
 
-    public static final String COREAPPS_CONDITIONLIST_APP = "coreapps.conditionlist";
+  public static final String COREAPPS_CONDITIONLIST_APP = "coreapps.conditionlist";
 
-    public static final String COREAPPS_LATEST_OBS_FOR_CONCEPT_LIST_APP = "coreapps.latestObsForConceptList";
+  public static final String COREAPPS_LATEST_OBS_FOR_CONCEPT_LIST_APP =
+      "coreapps.latestObsForConceptList";
 
-    public static final String COREAPPS_OBS_ACROSS_ENCOUNTERS_APP = "coreapps.obsAcrossEncounters";
+  public static final String COREAPPS_OBS_ACROSS_ENCOUNTERS_APP = "coreapps.obsAcrossEncounters";
 
-    public static final String COREAPPS_OBS_GRAPH_APP = "coreapps.obsGraph";
+  public static final String COREAPPS_OBS_GRAPH_APP = "coreapps.obsGraph";
 
-    public static final String COREAPPS_RELATIONSHIPS_APP = "coreapps.relationships";
+  public static final String COREAPPS_RELATIONSHIPS_APP = "coreapps.relationships";
 
-    public static final String COREAPPS_MOST_RECENT_VITALS_APP = "coreapps.mostRecentVitals";
+  public static final String COREAPPS_MOST_RECENT_VITALS_APP = "coreapps.mostRecentVitals";
 
-    public static final String COREAPPS_ACTIVE_VISITS_APP = "coreapps.activeVisits";
+  public static final String COREAPPS_ACTIVE_VISITS_APP = "coreapps.activeVisits";
 
-    public static final String APPOINTMENTSCHEDULINGUI_TAB_EXT = "appointmentschedulingui.tab";
+  public static final String APPOINTMENTSCHEDULINGUI_TAB_EXT = "appointmentschedulingui.tab";
 
-    public static final String PATIENT_DASHBOARD_APPOINTMENTS_EXT =
-            "org.openmrs.module." + "appointmentschedulingui.firstColumnFragments.patientDashboard.patientAppointments";
+  public static final String PATIENT_DASHBOARD_APPOINTMENTS_EXT =
+      "org.openmrs.module."
+          + "appointmentschedulingui.firstColumnFragments.patientDashboard.patientAppointments";
 
-    public static final String STICKY_NOTE_EXT =
-            "org.openmrs.module.coreapps.patientHeader" + ".secondLineFragments.stickyNote";
+  public static final String STICKY_NOTE_EXT =
+      "org.openmrs.module.coreapps.patientHeader" + ".secondLineFragments.stickyNote";
 
-    public static final String ACTIVE_VISIT_STATUS_EXT =
-            "org.openmrs.module.coreapps.patientHeader" + ".secondLineFragments.activeVisitStatus";
+  public static final String ACTIVE_VISIT_STATUS_EXT =
+      "org.openmrs.module.coreapps.patientHeader" + ".secondLineFragments.activeVisitStatus";
 
-    public static final String COREAPPS_CREATE_VISIT_EXT = "org.openmrs.module.coreapps.createVisit";
+  public static final String COREAPPS_CREATE_VISIT_EXT = "org.openmrs.module.coreapps.createVisit";
 
-    public static final String COREAPPS_CREATE_RETROSPECTIVE_VISIT_EXT =
-            "org.openmrs.module.coreapps." + "createRetrospectiveVisit";
+  public static final String COREAPPS_CREATE_RETROSPECTIVE_VISIT_EXT =
+      "org.openmrs.module.coreapps." + "createRetrospectiveVisit";
 
-    public static final String COREAPPS_MERGE_VISITS_EXT = "org.openmrs.module.coreapps.mergeVisits";
+  public static final String COREAPPS_MERGE_VISITS_EXT = "org.openmrs.module.coreapps.mergeVisits";
 
-    public static final String ALLERGYUI_PATIENT_DASHBOARD_EXT =
-            "org.openmrs.module.allergyui." + "patientDashboard.secondColumnFragments";
+  public static final String ALLERGYUI_PATIENT_DASHBOARD_EXT =
+      "org.openmrs.module.allergyui." + "patientDashboard.secondColumnFragments";
 
-    public static final String PATIENTFLAGS_EXT = "patientflags.patientDashboard.secondColumnFragments";
+  public static final String PATIENTFLAGS_EXT =
+      "patientflags.patientDashboard.secondColumnFragments";
 
-    public static final String ATTACHMENTS_EXT =
-            "org.openmrs.module.attachments.patientDashboard." + "secondColumnFragments.att";
+  public static final String ATTACHMENTS_EXT =
+      "org.openmrs.module.attachments.patientDashboard." + "secondColumnFragments.att";
 
-    public static final String ATTACHMENTS_OVERALL_ACTION_EXT = "attachments.attachments.overallActions.default";
+  public static final String ATTACHMENTS_OVERALL_ACTION_EXT =
+      "attachments.attachments.overallActions.default";
 
-    public static final String LOCATIONBASEDACCESS_EXT = "org.openmrs.module.locationbasedaccess.editLocation";
+  public static final String LOCATIONBASEDACCESS_EXT =
+      "org.openmrs.module.locationbasedaccess.editLocation";
 
-    public static final String COREAPPS_MARK_PATIENT_DECEASED_EXT = "org.openmrs.module.coreapps.markPatientDead";
+  public static final String COREAPPS_MARK_PATIENT_DECEASED_EXT =
+      "org.openmrs.module.coreapps.markPatientDead";
 
-    public static final String COREAPPS_DATA_MANAGEMENT_APP = "coreapps.dataManagementApp";
+  public static final String COREAPPS_DATA_MANAGEMENT_APP = "coreapps.dataManagementApp";
 
-    public static final String REGISTRATION_APP_EDIT_PATIENT_DASHBOARD_EXT =
-            "org.openmrs.module.registrationapp.editPatientDemographics";
+  public static final String REGISTRATION_APP_EDIT_PATIENT_DASHBOARD_EXT =
+      "org.openmrs.module.registrationapp.editPatientDemographics";
 
-    public static final String LOCATIONBASEDACCES_PATIENT_HEADER_LOCATION_EXT = "org.openmrs.module.locationbasedaccess.patientHeader.secondLineFragments.patientLocation";
+  public static final String LOCATIONBASEDACCES_PATIENT_HEADER_LOCATION_EXT =
+      "org.openmrs.module.locationbasedaccess.patientHeader.secondLineFragments.patientLocation";
 
-    private PatientDashboardAppsConstants() {
+  public static final String ADMINUI_LOCATION_GROUP_EXT =
+      "org.openmrs.module.adminui.locations.group";
+  public static final String ADMINUI_MANAGE_LOCATIONS_LINK_EXT =
+      "org.openmrs.module.adminui.manageLocations.link";
+  public static final String ADMINUI_MANAGE_LOCATION_TAGS_LINK_EXT =
+      "org.openmrs.module.adminui.manageLocationTags.link";
+  public static final String ADMINUI_MANAGE_LOCATION_ATTRIBUTE_TYPES_LINK_EXT =
+      "org.openmrs.module.adminui.manageLocationAttributeTypes.link";
+  public static final String ADMINUI_ENCOUNTERS_GROUP_EXT =
+      "org.openmrs.module.adminui.encounters.group";
+  public static final String ADMIN_UI_ENCOUNTERS_LINK_EXT = "org.openmrs.module.adminui.encounters";
+  public static final String ADMIN_UI_ENCOUNTER_ROLES_LINK_EXT =
+      "org.openmrs.module.adminui.manageEncounterRoles.link";
+  public static final String ADMINUI_VISITS_GROUP_EXT = "org.openmrs.module.adminui.visits.group";
+  public static final String ADMINUI_VISITS_LINK_EXT = "org.openmrs.module.adminui.visits";
+  public static final String ADMINUI_MANAGE_VISIT_ATTRIBUTE_TYPES_LINK_EXT =
+      "org.openmrs.module.adminui.manageVisitAttributeTypes.link";
+  public static final String ADMINUI_ROLES_AND_PRIVILIGES_GROUP_EXT =
+      "org.openmrs.module.adminui.rolesAndPrivileges.group";
+  public static final String ADMINUI_ROLES_LINK_EXT = "org.openmrs.module.adminui.roles";
+  public static final String ADMINUI_PRIVILIGES_LINK_EXT = "org.openmrs.module.adminui.Privileges";
+  public static final String ADMINUI_PROVIDERS_GROUP_EXT =
+      "org.openmrs.module.adminui.providers.group";
+  public static final String ADMINUI_MANAGE_PROVIDER_ATTRIBUTE_TYPES_LINK_EXT =
+      "org.openmrs.module.adminui.manageProviderAttributeTypes.link";
+  public static final String ADMINUI_PATIENT_FLAGS_GROUP_EXT =
+      "org.openmrs.module.adminui.patientflags.group";
+  public static final String ADMINUI_MANAGE_FLAGS_LINK_EXT =
+      "org.openmrs.module.patientflags.manageFlags.link";
 
-    }
+  public static final String ADMINUI_MANAGE_PATIENT_IDENTIFIER_TYPES_LINK_EXT =
+      "org.openmrs.module.adminui.managePatientIdentifierTypes.link";
+
+  public static final String OWA_CONCEPT_DICTIONARY_APP = "owa.conceptdictionary";
+  public static final String OWA_METADATA_MAPPING_APP = "owa.metadatamapping";
+  public static final String OWA_CONCEPT_LAB_APP = "owa.openconceptlab";
+
+  private PatientDashboardAppsConstants() {}
 }
