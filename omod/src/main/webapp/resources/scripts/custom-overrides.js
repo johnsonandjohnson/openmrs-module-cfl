1295700400ff3f5c5eac06dfe2705b81e88b78f5
druchniewicz <druchniewicz@soldevelo.com>
2024-01-17 18:45:36 +0100

    AGRE-3192: Override some labels on patient dashboard

--- omod/src/main/webapp/resources/scripts/custom-overrides.js
+++ omod/src/main/webapp/resources/scripts/custom-overrides.js
@@ -39,6 +39,7 @@
     overrideUserAccountLinks();
     redirectToCorrectFindPatientPage();
     updateBreadcrumbsInHtmlForms();
+    customizeForLeprosy();
   }
 
   function addBreadCrumbOnHomePage() {
@@ -235,6 +236,20 @@
     );
 
     manageVisitBreadcrumbElement.insertBefore(lastBreadcrumbElement);
+  }
+
+  function customizeForLeprosy() {
+    //replaces Patient label in Messages widget on patient dashboard with Portuguese translation
+    jqr('.time-label').text('Participante');
+
+    //overrides 'Paciente' occurrences with 'Participante' in delete patient dialog
+    const originalHeaderText = jqr('#delete-patient-creation-dialog .dialog-header h3').text();
+    const changedHeaderText = originalHeaderText.replace('Paciente', 'Participante');
+    jqr('#delete-patient-creation-dialog .dialog-header h3').text(changedHeaderText);
+
+    const originalBodyText = jqr('#delete-patient-creation-dialog .dialog-content p').text();
+    const changedBodyText = originalBodyText.replace('paciente', 'participante');
+    jqr('#delete-patient-creation-dialog .dialog-content p').text(changedBodyText);
   }
 
   function getPatientUuidParamFromURL() {
