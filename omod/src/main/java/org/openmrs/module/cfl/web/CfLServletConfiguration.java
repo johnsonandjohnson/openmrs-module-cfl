1b508988e144e70ab254edf339d22e4014e56c85
pwargulak <pwargulak@soldevelo.com>
2024-04-29 16:04:38 +0200

    93: Added workaround fix for mandatory password change after login (#7)

--- omod/src/main/java/org/openmrs/module/cfl/web/CfLServletConfiguration.java
+++ omod/src/main/java/org/openmrs/module/cfl/web/CfLServletConfiguration.java
@@ -2,6 +2,7 @@ package org.openmrs.module.cfl.web;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.cfl.filter.CflForcePasswordChangeFilter;
 import org.openmrs.module.cfl.filter.CflSecurityHeadersFilter;
 import org.springframework.web.context.ServletContextAware;
 
@@ -19,6 +20,7 @@ import java.util.EnumSet;
  * <ul>
  *   <li>Copies cflUncaughtException.jsp to root WEB-INF.
  *   <li>Configures security Filter.
+ *   <li>Adds CflForcePasswordChangeFilter which redirects to proper change password page.</li>
  * </ul>
  */
 public class CfLServletConfiguration implements ServletContextAware {
@@ -28,13 +30,23 @@ public class CfLServletConfiguration implements ServletContextAware {
   private static final Path CFL_UNCAUGHT_EXCEPTION_FILE = Paths.get("cflUncaughtException.jsp");
   private static final Path WEB_INF_VIEW_DIR = Paths.get("WEB-INF", "view");
 
+  private ServletContext servletContext;
+
   @Override
   public void setServletContext(ServletContext servletContext) {
-    copyCflUncaughtExceptionPage(servletContext);
-    addSecurityFilter(servletContext);
+    if (servletContext != null) {
+      this.servletContext = servletContext;
+      initServletContext();
+    }
   }
 
-  private void copyCflUncaughtExceptionPage(ServletContext servletContext) {
+  private void initServletContext() {
+    copyCflUncaughtExceptionPage();
+    addSecurityFilter();
+    addCflForceLoginFilter();
+  }
+
+  private void copyCflUncaughtExceptionPage() {
     Path basePath = Paths.get(servletContext.getRealPath(""));
     try {
       Path srcFile = basePath.resolve(MODULE_ROOT_DIR).resolve(CFL_UNCAUGHT_EXCEPTION_FILE);
@@ -45,13 +57,23 @@ public class CfLServletConfiguration implements ServletContextAware {
     }
   }
 
-  private void addSecurityFilter(ServletContext servletContext) {
+  private void addSecurityFilter() {
     try {
       servletContext
           .addFilter("CflSecurityHeadersFilter", new CflSecurityHeadersFilter())
           .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
     } catch (Exception e) {
       LOGGER.error("Failed to add CflSecurityHeadersFilter", e);
+    }
+  }
+
+  private void addCflForceLoginFilter() {
+    try {
+      servletContext
+          .addFilter("CflForcePasswordChangeFilter", new CflForcePasswordChangeFilter())
+          .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+    } catch (Exception e) {
+      LOGGER.error("Failed to add CflForcePasswordChangeFilter", e);
     }
   }
 }
